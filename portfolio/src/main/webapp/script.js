167045e40b6f05b3ee49cfb55eb131128596152e
Awad <aosmann7@gmail.com>
2020-06-04 16:52:02 +0000

    Switches commens to JSDocs format.

--- portfolio/src/main/webapp/script.js
+++ portfolio/src/main/webapp/script.js
@@ -40,6 +40,7 @@ function getRandomGreeting() {
 
 /**
  * Constructs the given array in a readable format for html page.
+ * @param {Array} - the list of comments
  */
 function printUserComments(comments){
   for(let index = 0; index < comments.length; index++){
